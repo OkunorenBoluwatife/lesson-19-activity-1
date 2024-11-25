3145fdda80bfc4974337e61d8afa4e09ffab9a6c
OkunorenBoluwatife <okunorenboluwatife@gmail.com>
2024-11-25 18:46:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("Numbers").
+        innerHTML = "1234567890";
+        document.getElementById("Alphabets").
+        innerHTML = "ABCD";
+        document.getElementById("Shapes").
+        innerHTML = "Circle";
